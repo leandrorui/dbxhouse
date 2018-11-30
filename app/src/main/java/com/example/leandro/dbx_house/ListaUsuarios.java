9a9ed3d97564053349928ac2f8199a4de4e7c418
leandroruisjbv <40581238+leandroruisjbv@users.noreply.github.com>
2018-11-29 23:08:47 -0200

    Mudanças na tela de listagem de usuario
    Quando a tela abre, aparecem os campos pra inserir usuário, senha, e um botão gravar, e a lista de "usuários" cadastrados.
    Quando o botão usuário recebe o foco, a lista fica invisível. Quando o campo senha perde o foco, a lista volta a ficar visível

--- app/src/main/java/com/example/leandro/dbx_house/ListaUsuarios.java
+++ app/src/main/java/com/example/leandro/dbx_house/ListaUsuarios.java
@@ -14,44 +14,49 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ListaUsuarios extends AppCompatActivity {
-    //ListView listaDeUsuarios = (ListView) findViewById(R.id.userview);
-    //
-    //EditText txtSenha = (EditText) findViewById(R.id.textViewGravaPasswd);
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //listaDeUsuarios.setVisibility(View.INVISIBLE);
         setContentView(R.layout.activity_lista_usuarios);
-
-        exibeListView();
-
-        EditText txtEdit = (EditText) findViewById(R.id.textViewGravaLogin);
-        txtEdit.setOnFocusChangeListener(new OnFocusChangeListener() {
-
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                mensagem("deu certo");
-            }
-        });
-
-    }
-
-    private void exibeListView() {
         String[] dados = new String[] { "Cupcake", "Donut", "Eclair", "Froyo", "Gingerbread",
                 "Honeycomb", "Ice Cream Sandwich", "Jelly Bean",
                 "KitKat", "Lollipop", "Marshmallow", "Nougat" };
 
         List<String> usuarios = Arrays.asList(dados);
-        ListView listview = (ListView) findViewById(R.id.userview);
+        final ListView listview = (ListView) findViewById(R.id.userview);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, dados);
         listview.setAdapter(adapter);
         listview.requestFocus();
-        listview.setVisibility(View.VISIBLE);
+        //listview.setVisibility(View.VISIBLE);
+
+        EditText edittxt= (EditText) findViewById(R.id.textViewGravaLogin);
+        edittxt.setOnFocusChangeListener(new OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    // code to execute when EditText loses focus
+                    listview.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        EditText editpass= (EditText) findViewById(R.id.textViewGravaPasswd);
+        editpass.setOnFocusChangeListener(new OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    // code to execute when EditText loses focus
+                    listview.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+
     }
+
 
     private void mensagem(String msg) {
         Toast toast = Toast.makeText(getApplicationContext(), msg,Toast.LENGTH_SHORT);
